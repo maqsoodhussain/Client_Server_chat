4614c5693ac057316958436747c426661653cc16
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2024-06-24 14:16:58 +0530

    exe files

--- FINAL 1.0/generateKeys/genkey.py
+++ FINAL 1.0/generateKeys/genkey.py
@@ -1,15 +1,14 @@
 from Crypto.PublicKey import RSA
-
 key = RSA.generate(2048)
 def privatekey():
     private_key = key.export_key()
-    file_out = open('generateKeys/public.key','wb')
+    file_out = open('public.key','wb')
     file_out.write(private_key)
     file_out.close
 
 def publicKey():
     public_key = key.publickey().export_key()
-    file_out = open("generateKeys/private.key",'wb')
+    file_out = open("gprivate.key",'wb')
     file_out.write(public_key)
     file_out.close()
     
